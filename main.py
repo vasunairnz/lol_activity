c2406ebf46d5e34d41c3b698a38d49a2ce0387f3
Vasu Nair <vasunairnz@gmail.com>
2026-04-11 11:49:14 +1200

    fixed the requests syntax, set up all needed requests, extracted desired data from json of requests (mode, time, kda, cs, champ)

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import requests
+# API info at https://developer.riotgames.com/docs/lol
 
-response = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata")
 
-print(response.status_code)
-print(response.text)
-print(response.json())
+all_game_data = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)       
+#print(all_game_data.status_code)
+
+player_score = requests.get("https://127.0.0.1:2999/liveclientdata/playerscores?riotId=", params = {"riotId": "Vasu#OC"}, verify = False)
+#print(player_score.status_code)
+
+game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+#print(game_stats.status_code)
+
+
+
+game_mode = game_stats.json()["gameMode"]
+
+game_time = game_stats.json()["gameTime"]
+
+champion_name = all_game_data.json()["activePlayer"]["abilities"]["E"]["id"]
+champion_name = champion_name[:-1]
+
+kills = player_score.json()["kills"]
+
+deaths = player_score.json()["deaths"]
+
+assists = player_score.json()["assists"]
+
+creep_score = player_score.json()["creepScore"]
+
+
+print(game_mode)
+print(game_time)
