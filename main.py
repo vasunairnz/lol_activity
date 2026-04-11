6317c71ed8cf7d8017c41f6c5ee89b2db7a7369a
Vasu Nair <vasunairnz@gmail.com>
2026-04-11 16:04:38 +1200

    finished the backend. Set up loops for in and out of game. Set up overall loop so it checks for wether in game every 10 seconds. When in game checks live data every 2 secs.

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
+import time
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import requests
 # API info at https://developer.riotgames.com/docs/lol
 
+# 2 states  "in_game" and "out_of_game"
+state = "out_of_game"
 
-all_game_data = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)       
-#print(all_game_data.status_code)
+#overall Loop
+while True:
+    if state == "out_of_game":
+        try:
+            game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+            game_mode = game_stats.json()["gameMode"]
+        except:
+            print(f"[{time.ctime()}]  still out of game")
+            time.sleep(10)
+            continue
+        state = "in_game"   # if exception didnt occur -> ingame -> change state
 
-player_score = requests.get("https://127.0.0.1:2999/liveclientdata/playerscores?riotId=", params = {"riotId": "Vasu#OC"}, verify = False)
-#print(player_score.status_code)
+    
+    elif state == "in_game":
 
-game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
-#print(game_stats.status_code)
-
-
-
-game_mode = game_stats.json()["gameMode"]
-
-game_time = game_stats.json()["gameTime"]
-
-champion_name = all_game_data.json()["activePlayer"]["abilities"]["E"]["id"]
-champion_name = champion_name[:-1]
-
-kills = player_score.json()["kills"]
-
-deaths = player_score.json()["deaths"]
-
-assists = player_score.json()["assists"]
-
-creep_score = player_score.json()["creepScore"]
+        #initial call to get champion, gamemode data 
+        try:
+            all_game_data = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)       
+            game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+        except:
+            state = "out_of_game" # if fail to get data -> out of game -> continue back to waiting
+            continue
 
 
-print(game_mode)
-print(game_time)
+        # extract gamemode and champ name from the response json
+        game_mode = game_stats.json()["gameMode"]
+        for player in all_game_data.json()["allPlayers"]:
+            if player["riotId"] == "Vasu#OC":
+                champion_name = player["championName"]
+                break
+
+
+        #repeated call for live kda, cs, time
+        while True:       
+            try:
+                player_score = requests.get("https://127.0.0.1:2999/liveclientdata/playerscores?riotId=", params = {"riotId": "Vasu#OC"}, verify = False)
+
+                game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+            except:
+                state = "out_of_game" # if fail to get data -> out of game -> break the loop to return back to waiting
+                print("game is over/quit")
+                break
+
+
+            game_time = game_stats.json()["gameTime"]
+
+            kills = player_score.json()["kills"]
+
+            deaths = player_score.json()["deaths"]
+
+            assists = player_score.json()["assists"]
+
+            creep_score = player_score.json()["creepScore"]
+            
+
+            print(f"{time.ctime()}  {kills}/{deaths}/{assists} | cs = {creep_score} | {champion_name} | {game_mode}")
+
+            time.sleep(2)
