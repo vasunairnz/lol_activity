33fd3ac2ccef7c546a94dffa0f2dbc1a4c019980
Vasu Nair <vasunairnz@gmail.com>
2026-04-15 10:40:40 +1200

    added front end discord activity section. downloaded all splash arts. TBD: set up connection to splash arts

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
 import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import requests
+from pypresence import Presence
+from pypresence.types import StatusDisplayType
 # API info at https://developer.riotgames.com/docs/lol
 
-#GLOBAL RIOT ID USED FOR APP
-riot_id = "Vasu#OC"
+riot_id = "Vasu#OC" # GLOBAL RIOT ID USED FOR APP
+state = "out_of_game" # 2 states  "in_game" and "out_of_game"
 
+# initialise discord rich presene connection
+rpc = Presence(os.getenv("DISCORD_CLIENT_ID")) #id from discrd dev portal
+rpc.connect()
 
-# 2 states  "in_game" and "out_of_game"
-state = "out_of_game"
 
 #overall Loop
 while True:
+    
+
     if state == "out_of_game":
         try:
             game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
@@ -23,6 +31,8 @@ while True:
             continue
         state = "in_game"   # if exception didnt occur -> ingame -> change state
 
+
+
     
     elif state == "in_game":
 
@@ -30,41 +40,55 @@ while True:
         try:
             all_game_data = requests.get("https://127.0.0.1:2999/liveclientdata/allgamedata", verify = False)       
             game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+            
+            game_mode = game_stats.json()["gameMode"]
+            
+            champion_name = None
+            for player in all_game_data.json()["allPlayers"]:
+                if player["riotId"] == riot_id:
+                    champion_name = player["championName"]
+                    break
+            
+            if champion_name is None:
+                state = "out_of_game"
+                rpc.clear()
+                continue
+
         except:
             state = "out_of_game" # if fail to get data -> out of game -> continue back to waiting
+            rpc.clear()
             continue
 
 
         # extract gamemode and champ name from the response json
-        game_mode = game_stats.json()["gameMode"]
-        for player in all_game_data.json()["allPlayers"]:
-            if player["riotId"] == riot_id:
-                champion_name = player["championName"]
-                break
-
+        
 
         #repeated call for live kda, cs, time
         while True:       
             try:
                 player_score = requests.get("https://127.0.0.1:2999/liveclientdata/playerscores?riotId=", params = {"riotId": riot_id}, verify = False)
+                player_score_data = player_score.json()
 
-                game_stats = requests.get("https://127.0.0.1:2999/liveclientdata/gamestats", verify = False)
+                kills = player_score_data["kills"]
+                deaths = player_score_data["deaths"]
+                assists = player_score_data["assists"]
+                creep_score = player_score_data["creepScore"]
+            
             except:
                 state = "out_of_game" # if fail to get data -> out of game -> break the loop to return back to waiting
                 print("game is over/quit")
+                rpc.clear()
                 break
-
-
-            game_time = game_stats.json()["gameTime"]
-
-            kills = player_score.json()["kills"]
-
-            deaths = player_score.json()["deaths"]
-
-            assists = player_score.json()["assists"]
-
-            creep_score = player_score.json()["creepScore"]
             
+
+            rpc.update(
+                status_display_type=StatusDisplayType.STATE,
+                state=f" {kills}/{deaths}/{assists}   |   cs = {creep_score}",
+                details= f"{champion_name}",
+                name= f"{game_mode}",
+                # large_image = f"{champion_name}",  set up later!!!
+                # large_text= f"{champion_name}",    set up later!!!
+                )
 
             print(f"{time.ctime()}  {kills}/{deaths}/{assists} | cs = {creep_score} | {champion_name} | {game_mode}")
 
